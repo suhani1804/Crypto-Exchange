a461119646c8ea286f8ee48fcfdc4f2640da8781
suhani1804 <107800177+suhani1804@users.noreply.github.com>
2023-04-09 23:53:44 +0530

    update

--- client/src/component/Transaction.jsx
+++ client/src/component/Transaction.jsx
@@ -2,17 +2,49 @@ import React, { useContext } from "react";
 
 import { TransactionContext } from "../context/TransactionContext";
 
-// import useFetch from "../hooks/useFetch";
-import dummyData from "../utils/dummyData";
-// import { shortenAddress } from "../utils/shortenAddress";
+import dummyData from "../Utils/dummyData"
+import { shortenAddress } from "../Utils/shortenAddress";
+
+export const TransactionCard = ({addressTo, addressFrom, timestamp,message,amount,url,keyword})=>
+{
+  return(
+    <div className="bg-[#181918] m-4 flex flex-1
+    2xl:min-w-[450px]
+    2xl:max-w-[500px]
+    sm:min-w-[270px]
+    sm:max-w-[300px]
+    flex-xol p-3 rounded-md hover:shadow-2xl
+    ">
+      <div className="flex flex-col items-center w-full mt-3">
+        <div className="flex justify-center w-full mb-6 p-2">
+          <a href={`https://goerli.etherscan.io/address/${addressFrom}`} target="blank" rel="noopener noreferrer">
+            <p className="text-white">From:{shortenAddress(addressFrom)}</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+  
+}
+  
+
 const Transactions =()=>
 {
+  
+  const {transactions,currentAccount}=useContext(TransactionContext);
   return (
-    <div className="bg-[#181918] m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px]  sm:max-w-[300px] min-w-full flex-col p-3 rounded-md hover:shadow-2xl">
-      <div className="flex flex-col items-center w-full mt-3">
-        <div className="display-flex justify-start w-full mb-6 p-2">
-          
-        </div>
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex felx-col md:p-12 py-12 px-4 "> 
+      {currentAccount ?
+      (
+        <h3 className="text-white text-3xl text-center my-2">Latest Transaction</h3>
+      ):( <h3 className="text-white text-3xl text-center my-2">Connect wallet to see transactions Transaction</h3>)};
+      <div className="flex flex-wrap justify-center items-center mt-10">
+        {[dummyData].reverse().map((transactions,i)=>
+        {
+          <TransactionCard key={i} {...transactions}/>
+        })}
+      </div>
       </div>
     </div>
   );
