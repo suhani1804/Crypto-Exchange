3947a98863bb1e9eb2cb78583c936c2d5acd5fe5
Prabhjot <777prabhjot@gmail.com>
2023-04-16 12:46:50 +0530

    created footer section

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -1,7 +1,35 @@
-const Footer =() =>
-{
-    return (
-        <h1>Footer</h1>
-    )
-}
-export default Footer
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footerContainer">
+        <div className="row">
+          <div className="footer-col">
+            <h4>company</h4>
+            <ul>
+              <li>
+                <a href="#">about us</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>follow us</h4>
+            <div className="social-links">
+              <a href="#" target="_blank">
+                <BsGithub size={25}/>
+              </a>
+              <a href="#" target="_blank">
+                <BsLinkedin size={25} color="#0A66C2"/>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
